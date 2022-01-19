c15bd9536a0d6b78154a4203fdf2b238b0c106d1
CarlosDevops2530 <carloscuervo2011@gmail.com>
2022-01-19 13:24:56 -0500

    Ruta agregada

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let nombre = document.getElementById('name').value
+let password = document.getElementById('pass')
+let boton = document.getElementById('boton')
+
+function entrar () {
+    if (password.value === '123') {
+        alert('Bienvenido a PGIRHS')
+        window.location.href = 'https://carlosdevops2530.github.io/pgirhs-2/PaginaPrincipal/index2.html'
+    } else {
+        alert('contraseña incorrecta')
+        return false
+    }
+
+    console.log(window.location)
+}
+boton.addEventListener('click', entrar)
