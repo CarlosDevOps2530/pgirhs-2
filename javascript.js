b1ac2601b901344dfd1c9f2177b611d3d44b8174
CarlosDevops2530 <carloscuervo2011@gmail.com>
2022-01-20 18:38:17 -0500

    Ultimos CAMBIOS

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
-let nombre = document.getElementById('name').value
+let nombre = document.getElementById('name')
 let password = document.getElementById('pass')
 let boton = document.getElementById('boton')
 
 function entrar () {
+    if (nombre.value === "" || password.value === "") {
+        alert('Diligencia Todos Los Campos')
+        return false
+    }
+
     if (password.value === '123Car') {
         alert('Bienvenido a PGIRHS')
         window.location.href = 'https://carlosdevops2530.github.io/pgirhs-2/PaginaPrincipal/index2.html'
@@ -10,6 +15,8 @@ function entrar () {
         alert('contraseña incorrecta')
         return false
     }
+
+    
 
     console.log(window.location)
 }
